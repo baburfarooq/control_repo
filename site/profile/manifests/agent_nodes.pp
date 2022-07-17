37f0e957e509073ad721f20cd8a3564376ca2a5d
Babur Farooq <babur.farooq@gmail.com>
2022-07-17 12:15:22 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-  docketagent::node{'web.puppet.vm':}
-  docketagent::node{'db.puppet.vm':}
+  docketagent::node {'web.puppet.vm':}
+  docketagent::node {'db.puppet.vm':}
 }
