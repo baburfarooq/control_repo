df0ce8855bd484ae3345f97199488fff4ab1898b
Babur Farooq <babur.farooq@gmail.com>
2022-07-17 12:37:45 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,20 @@
 node default {
+  file {'/root/README':
+    ensure => file,
+    content => 'this is readme',
+    owner => 'root',
+  }
 }
 node 'master.puppet.vm' {
 include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
 }
 
 
